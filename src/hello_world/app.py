2bc8854d3e8006400d3723e419a8ede498ff8227
Mr-i-me <Mr-i-me@Ivans-MacBook-Pro.local>
2023-02-01 18:28:26 -0300

    deploy

--- src/hello_world/app.py
+++ src/hello_world/app.py
@@ -1,22 +1,12 @@
-from http import HTTPStatus
-
-from helpers.handler_base import Handler, Result
-from helpers.hw_helper import helper_fn
-
-
-class HelloWorld(Handler):
-
-    def handler(self):
-        # return {
-        #     "statusCode": 200,
-        #     "body": json.dumps({
-        #         "message": "hello world",
-        #         # "location": ip.text.replace("\n", "")
-        #     }),
-        # }
-        return Result(HTTPStatus.OK, {"message": "hello world"})
+import json
 
 
 def lambda_handler(event, context):
-    helper_fn()
-    return HelloWorld(event, context).run()
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "hello world",
+            # "location": ip.text.replace("\n", "")
+        }),
+    }
+
