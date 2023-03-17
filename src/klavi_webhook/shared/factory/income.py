9932d25af577c4a5a5bd1938b285fb050479e421
Silvio Lucena Junior <silvio_lucena_junior@hotmail.com>
2023-03-17 15:18:28 -0300

    fix income payload

--- src/klavi_webhook/shared/factory/income.py
+++ src/klavi_webhook/shared/factory/income.py
@@ -4,16 +4,18 @@ from shared.models.income import Income, IncomeStream, IncomeTransaction
 def build_income_from_kavli_payload(payload):
     income = Income(**parse_income_payload_body(payload))
 
-    for income_stream_payload in payload['incomeStream']:
-        income_stream = IncomeStream(**parse_income_payload_income_stream(income_stream_payload))
-        income_stream.category_id = income.id
+    if payload['incomeStream'] is not None:
+        
+        for income_stream_payload in payload['incomeStream']:
+            income_stream = IncomeStream(**parse_income_payload_income_stream(income_stream_payload))
+            income_stream.category_id = income.id
 
-        for income_transaction_payload in income_stream_payload['incomeTransactions']:
-            transaction_detail = IncomeTransaction(**parse_income_payload_income_transaction(income_transaction_payload))
-            transaction_detail.category_id = income_stream.id
-            income_stream.income_transactions.append(transaction_detail)
+            for income_transaction_payload in income_stream_payload['incomeTransactions']:
+                transaction_detail = IncomeTransaction(**parse_income_payload_income_transaction(income_transaction_payload))
+                transaction_detail.category_id = income_stream.id
+                income_stream.income_transactions.append(transaction_detail)
 
-        income.income_streams.append(income_stream)
+            income.income_streams.append(income_stream)
 
     return income
 
